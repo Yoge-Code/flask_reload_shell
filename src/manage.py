387de8fd38e7040b366eec409218a2a770ad2594
YoGe <ygguang1992@gmail.com>
2020-01-30 12:30:12 +0800

    fix

--- src/manage.py
+++ src/manage.py
@@ -7,9 +7,21 @@ import os
 from app import create_app
 from flask_script import Manager, Shell, Server
 
+from werkzeug.serving import run_with_reloader
+from functools import partial
+
+
+class ReloaderShell(Shell):
+
+    def run(self, *args, **kwargs):
+        return run_with_reloader(partial(super().run, *args, **kwargs))
+
+
 app = create_app()
 manager = Manager(app)
+manager.add_command("shell", ReloaderShell(make_context=lambda : dict(app=app)))
 manager.add_command("run", Server(host="0.0.0.0", port=8088))
+
 
 def make_shell_context():
     return dict(app=app)
