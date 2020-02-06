2c95529ba23162afd3d93e54ce07605332e93cac
YoGe <ygguang1992@gmail.com>
2020-02-06 12:02:38 +0800

    fix app context

--- src/manage.py
+++ src/manage.py
@@ -9,17 +9,27 @@ from flask_script import Manager, Shell, Server
 
 from werkzeug.serving import run_with_reloader
 from functools import partial
+from flask.ctx import AppContext
 
 
 class ReloaderShell(Shell):
 
+    def __init__(self, app):
+        self.app = app
+        super().__init__()
+
     def run(self, *args, **kwargs):
+        print(args, kwargs)
         return run_with_reloader(partial(super().run, *args, **kwargs))
+
+    def get_context(self):
+        AppContext(self.app).push()
+        return dict(app=app)
 
 
 app = create_app()
 manager = Manager(app)
-manager.add_command("shell", ReloaderShell(make_context=lambda : dict(app=app)))
+manager.add_command("shell", ReloaderShell(app))
 manager.add_command("run", Server(host="0.0.0.0", port=8088))
 
 
